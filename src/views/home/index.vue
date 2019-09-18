52266b3bf86d03d2c9ae87b73209e0214314f631
tao <1264994050@qq.com>
2019-09-18 18:38:10 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>我是主页</div>
+<!-- 先定义一个大容器 -->
+  <el-container>
+      <!-- 放置左侧 -->
+      <el-aside></el-aside>
+      <!-- 右侧大容器 -->
+      <el-container>
+        <!-- 头部 -->
+          <el-header></el-header>
+          <!-- 中间 -->
+          <el-main></el-main>
+      </el-container>
+  </el-container>
 </template>
 
 <script>
