fb3d23ba6ae0c27f17572df89c2f0733258975e7
tao <1264994050@qq.com>
2019-09-20 18:57:46 +0800

    黑马头条PC-主页模块-头部结构及下拉菜单

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,53 @@
+<template>
+<!-- 头部最外侧放一个el-row -->
+  <el-row class="layout-header" type="flex" justify="space-between" align="middle">
+      <!-- 左侧 -->
+      <el-col :span="6">
+          <i class="el-icon-s-unfold"></i>
+          <span class="title">江苏传智播客教育科技股份有限公司</span>
+      </el-col>
+      <!-- 右侧 -->
+      <el-col :span="4">
+          <img class="header-img" src="../../assets/img/avatar.jpg" alt="">
+          <!-- 下拉菜单组件 -->
+      <el-dropdown trigger="click">
+          <!-- 匿名插槽 -->
+         <span class="el-dropdown-link">
+        83期<i class="el-icon-arrow-down el-icon--right"></i>
+         </span>
+         <!-- 具名插槽 -->
+         <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item >个人信息</el-dropdown-item>
+         <el-dropdown-item >git地址</el-dropdown-item>
+         <el-dropdown-item >退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+      </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.layout-header{
+    height: 50px;
+    .el-icon-s-unfold{
+        font-size: 22px;
+    }
+    .title{
+        vertical-align: top;
+        margin-left: 5px;
+    }
+    .header-img{
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        vertical-align:middle;
+        margin-right: 4px;
+    }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,13 +2,15 @@
 <!-- 先定义一个大容器 -->
   <el-container>
       <!-- 放置左侧 -->
-      <el-aside>
+      <el-aside style="width:200px">
         <layout-aside></layout-aside>
       </el-aside>
       <!-- 右侧大容器 -->
       <el-container>
         <!-- 头部 -->
-          <el-header></el-header>
+          <el-header>
+            <layout-header></layout-header>
+          </el-header>
           <!-- 中间 -->
           <el-main></el-main>
       </el-container>
@@ -17,9 +19,11 @@
 
 <script>
 import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
 export default {
   components: {
-    'layout-aside': layoutAside
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
   }
 }
 </script>
