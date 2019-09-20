ecf08acf81af6d16d2f744803e462cae840112cf
tao <1264994050@qq.com>
2019-09-20 20:54:24 +0800

    黑马头条PC-主页模块-头部信息查询

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -8,12 +8,12 @@
       </el-col>
       <!-- 右侧 -->
       <el-col :span="4">
-          <img class="header-img" src="../../assets/img/avatar.jpg" alt="">
+           <img class="head-img" :src="userInfo.photo?userInfo.photo: defaultImg" alt="">
           <!-- 下拉菜单组件 -->
       <el-dropdown trigger="click">
           <!-- 匿名插槽 -->
          <span class="el-dropdown-link">
-        83期<i class="el-icon-arrow-down el-icon--right"></i>
+        {{userInfo.name}}<i class="el-icon-arrow-down el-icon--right"></i>
          </span>
          <!-- 具名插槽 -->
          <el-dropdown-menu slot="dropdown">
@@ -28,7 +28,28 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      userInfo: {}, // 个人信息对象
+      defaultImg: require('../../assets/img/avatar.jpg') // 转成base64
+    }
+  },
+  methods: {
+    // 获取用户个人资料
+    getUserInfo () {
+      let token = window.localStorage.getItem('user-token') // 从前端缓存中获取token
+      this.$axios({
+        url: '/user/profile',
+        headers: { 'Authorization': `Bearer ${token}` } // 请求参数
+      }).then(result => {
+        this.userInfo = result.data.data // 接收数据对象
+      })
+    }
+  },
+  // 钩子函数
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
@@ -42,7 +63,7 @@ export default {
         vertical-align: top;
         margin-left: 5px;
     }
-    .header-img{
+    .head-img{
         width: 40px;
         height: 40px;
         border-radius: 50%;
