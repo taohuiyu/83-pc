2244cb1fa265ee854a36d4ec76192c0fb1c8c7d0
tao <1264994050@qq.com>
2019-09-22 11:45:58 +0800

    黑马头条PC-评论列表-新建页面-挂载路由

--- src/router.js
+++ src/router.js
@@ -1,37 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/home/index.vue'
-import Login from './views/login'
+// import Home from './views/home/index.vue' // 完整写法
+import Home from './views/home/'
+import Login from './views/login' // 简写
 import Main from './views/home/main'
 Vue.use(Router)
-
 export default new Router({
   routes: [
+    // 一级路由
     {
       path: '/',
       redirect: '/home'
     },
     {
       path: '/home',
-      // name: 'home',
       component: Home,
       children: [
         {
-          path: '',
-          component: Main
-        }
-      ]
-    }, {
-      path: '/login',
-      component: Login
+          path: '', // 什么都不写 就默认是二级路由组件的
+          component: Main // 默认二级路由
+        }]
+    },
+    {
+      // path:'/home/comment', // 完整写法
+      path: 'comment', // 评论列表路径
+      component: () => import('./views/comment')
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
   ]
-})
+},
+{
+  path: '/login',
+  component: Login
+}
+)
