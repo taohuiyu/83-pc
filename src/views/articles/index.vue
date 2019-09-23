ea3d878b4f8eaffa439651f2a377095bb03267bc
tao <1264994050@qq.com>
2019-09-23 19:02:29 +0800

    黑马头条PC-内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,17 +1,39 @@
 <template>
   <el-card>
-      <bread-crumb slot="header">
+    <bread-crumb slot="header">
       <template slot="title">内容列表</template>
-      </bread-crumb>
+    </bread-crumb>
+    <el-form style="margin-left:50px">
+      <!-- 文章状态 -->
+      <el-form-item label="文章状态 :">
+        <el-radio-group>
+          <el-radio>全部</el-radio>
+          <el-radio>草稿</el-radio>
+          <el-radio>待审核</el-radio>
+          <el-radio>审核通过</el-radio>
+          <el-radio>审核失败</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <!-- 频道列表 -->
+      <el-form-item label="频道列表 :">
+        <el-select></el-select>
+      </el-form-item>
+      <!-- 时间 -->
+      <el-form-item label="时间选择 ：">
+        <el-date-picker
+          v-model="value1"
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+    </el-form>
   </el-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
